1a8cd1dafc5e462d49321235900cec2fa1bbb331
noproblem2311 <quanpva.dev@gmail.com>
2024-02-19 20:27:57 +0700

    authentication

--- SharedClassLibrary/obj/Debug/net8.0/SharedClassLibrary.AssemblyInfo.cs
+++ SharedClassLibrary/obj/Debug/net8.0/SharedClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f7e3f04e8e0f7bf456ccff47b4c4b095654ef20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fc2b0f6a63a20a01597b1f98766cb14da5ea9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
