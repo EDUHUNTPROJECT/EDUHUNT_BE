2f94e0bfe3722d90a74b8a56c6ec88c2fee16049
MathNumber <ducpd82@gmail.com>
2024-02-27 23:22:07 +0700

    add certificate

--- EDUHUNT_BE/Model/Certificate.cs
+++ EDUHUNT_BE/Model/Certificate.cs
@@ -9,12 +9,8 @@ namespace EDUHUNT_BE.Model
     public class Certificate
     {
         public Guid Id { get; set; }
-
-        public Guid UserId { get; set; }
-
+        public string UserId { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-
         public string ContentURL { get; set; }
     }
 }
